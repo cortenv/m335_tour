78394dcdfda9c37dacf77c138b8a695629d6e4ae
Vera Corten <vera.corten@hotmail.com>
2024-05-28 11:35:43 +0200

    task 2 - extras

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -6,8 +6,8 @@ export const HEROES: Hero[] = [
   { id: 14, name: 'Celeritas', superpower: 'hearing' },
   { id: 15, name: 'Magneta', superpower: 'telekinesis' },
   { id: 16, name: 'RubberMan', superpower: 'smell' },
-  { id: 17, name: 'Dynama', superpower: 'time' },
-  { id: 18, name: 'Dr. IQ', superpower: 'teleportation' },
-  { id: 19, name: 'Magma', superpower: 'fire' },
-  { id: 20, name: 'Tornado', superpower: 'ice' },
+  { id: 17, name: 'Dynama'},
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
 ];
